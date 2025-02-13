163b8cd43115542360a5999bc7cf90aa0f3c27e2
Nitin Verma <149617649+nv-70@users.noreply.github.com>
2025-02-13 04:56:41 +0000

    basic datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all Js code as newer version
+//alert(3+3) // it will give error because we are using node.js not browser
+console.log(3+3); console.log("nitin") // not good code readability
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// PRIMITIVE DATA TYPE
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol = unique 
+
+//OBJECT
+
+console.log(typeof isLoggedIn);// boolean type
+console.log(typeof name);   // string type
+console.log(typeof null); // object type
+console.log(typeof undefined ); // undefined type
